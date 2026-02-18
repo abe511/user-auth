6f40d912bf3f316501c0aae55e88902a4793ba8c
Art <absolutus11@gmail.com>
2026-02-19 03:22:35 +0400

    fix: add json limits

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import { userRouter } from "./routes/user.routes";
 
 const app = express();
 
-const PORT = process.env.POR || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({limit: "123kb"}));
 
 app.get("/", (req, res) => {
     res.send("all ok");
 })
 
-app.use("/api/users", userRouter);
+app.use("/api/users", express.json({limit: "11kb"}), userRouter);
 
 
 app.listen(PORT, () => {
