959c9334e32309f4838cd07062df4383d4e0b6dc
Art <absolutus11@gmail.com>
2026-02-24 18:51:25 +0400

    feat: initialize db with default admin

--- src/utils/db/initDB.mjs
+++ src/utils/db/initDB.mjs
@@ -1,6 +1,7 @@
 import path from "path";
 import { access, constants, readFile } from "fs/promises";
 import { Client } from "pg";
+import bcrypt from "bcrypt";
 
 export const db = new Client({
     connectionString: process.env.DATABASE_URL,
@@ -15,10 +16,27 @@ const initializeDB = async (filename) => {
         if(!sql) {
             throw new Error("DB initialization failed. SQL file is empty");
         }
+
+        // admin creds
+        const adminEmail = "admin@domain.com";
+        const adminPassword = "password123";
+        const hashedPassword = await bcrypt.hash(adminPassword, parseInt(process.env.SALT_ROUNDS));
+        
         // initialize the db
         await db.connect();
         console.log("DB connection established");
+        
         await db.query(sql);
+        
+        // add admin
+        await db.query(
+            `INSERT INTO users (fullname, birthdate, email, password, role) 
+            VALUES ('Default Admin', '1901-01-01', $1, $2, 'admin')`,
+            [adminEmail, hashedPassword]
+        );
+
+        console.log(`DB default user with role *admin* added. Email: ${adminEmail} Password: ${adminPassword}`);
+        
         console.log("DB initialized successfully");
     } catch(error) {
         console.error("DB initialization error:", error.message);
